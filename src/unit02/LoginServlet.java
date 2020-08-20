c849d502be173c3bdc50d566b52d54ec85b94349
teacher <net94@naver.com>
2020-08-20 12:15:03 +0900

    g

--- src/unit02/LoginServlet.java
+++ src/unit02/LoginServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sun.rmi.server.Dispatcher;
-
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
